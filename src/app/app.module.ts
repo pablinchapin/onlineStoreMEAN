1817aeb61805a4cca404868394c0bc12fc58b071
pablinchapin <pablinchapin@gmail.com>
2018-04-25 09:48:02 -0600

    Primer commit onlineStore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,40 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { RestApiService } from './rest-api.service';
+import { DataService } from './data.service';
+import { AuthGuardService } from './auth-guard.service';
+
+import { HomeComponent } from './home/home.component';
+import { MessageComponent } from './message/message.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MessageComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ProfileComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RestApiService, DataService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
